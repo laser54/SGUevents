059aa1a3d246765559601d4997f0f55330fcf362
alexander <likov.shura03@mail.ru>
2024-03-25 22:19:13 +0700

    secret and styles

--- SGUevents/settings.py
+++ SGUevents/settings.py
@@ -1,9 +1,9 @@
 import os
 
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 from pathlib import Path
 
-# load_dotenv()
+load_dotenv()
 
 
 
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
