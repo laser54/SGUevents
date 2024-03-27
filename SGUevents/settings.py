78c12a5d026b52c34ff8e1f515fbcac694ded58e
La5er <larinserg@gmail.com>
2024-03-28 00:04:37 +0700

    test deploy

--- SGUevents/settings.py
+++ SGUevents/settings.py
@@ -142,3 +142,5 @@ STATICFILES_DIRS: list[Path] =  [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
