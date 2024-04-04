915a0237a74737fe52ad7c7d9f5de6ee0b03f92a
la5er <larinserg@gmail.com>
2024-04-04 16:54:53 +0700

    user model improvement

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
-from .models import User  # Импортируем модель пользователя
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
-# Определяем класс администратора для настройки отображения модели в админ-панели
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'middle_name', 'department_id', 'telegram_id')  # Поля, которые будут отображаться в списке объектов
-    search_fields = ('first_name', 'last_name', 'middle_name', 'department_id', 'telegram_id')  # Поля, по которым будет работать поиск
-    list_filter = ('department_id',)  # Фильтры справа по указанным полям
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'middle_name', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (_('Custom fields'), {'fields': ('department_id', 'telegram_id')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'middle_name', 'email')}),
+        (_('Custom fields'), {'fields': ('department_id', 'telegram_id')}),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'middle_name', 'department_id', 'telegram_id', 'is_staff')
+    search_fields = ('username', 'first_name', 'last_name', 'middle_name', 'email', 'department_id', 'telegram_id')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups', 'department_id')
 
-# Регистрируем модель User с настройками UserAdmin
 admin.site.register(User, UserAdmin)
--- users/forms.py
+++ users/forms.py
@@ -2,14 +2,13 @@ from django import forms
 from .models import User
 
 class RegistrationForm(forms.ModelForm):
-    # telegram_id = forms.CharField(required=False, widget=forms.HiddenInput())
     class Meta:
         model = User
         fields = ['last_name', 'first_name', 'middle_name', 'department_id', 'telegram_id']
         widgets = {
             'middle_name': forms.TextInput(attrs={'placeholder': 'Отчество (необязательно)'}),
             'department_id': forms.NumberInput(attrs={'placeholder': 'ID отдела'}),
-            'telegram_id': forms.HiddenInput(),  # Указываем, что для telegram_id нужно использовать скрытый виджет
+            'telegram_id': forms.HiddenInput(),  # Скрытое поле для telegram_id
         }
         labels = {
             'first_name': 'Имя',
@@ -18,6 +17,15 @@ class RegistrationForm(forms.ModelForm):
             'department_id': 'ID отдела',
         }
 
+    def clean_telegram_id(self):
+        telegram_id = self.cleaned_data.get('telegram_id')
+        # Валидация поля telegram_id на предмет наличия значения для обычных пользователей
+        if not telegram_id:
+            raise forms.ValidationError("Требуется идентификатор Telegram для регистрации.")
+        return telegram_id
+
     def clean_department_id(self):
         department_id = self.cleaned_data.get('department_id')
+        if not department_id:
+            raise forms.ValidationError("Требуется указать ID отдела.")
         return department_id
