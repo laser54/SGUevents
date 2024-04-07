4a141ac0b3255792a52a85d3926ff53d03605485
La5er <larinserg@gmail.com>
2024-04-07 20:20:46 +0700

    login page

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
+from .views import login_view
 
 app_name = 'users'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('login/', views.log_in, name='login'),
+    path('login/', login_view, name='login'),
     # path('signup/', views.sign_up, name='signup'),
     path('register/', views.register, name='register'),
 ]
