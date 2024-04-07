08e06b6880c27936ba7df3656045a7d3d7a85f16
La5er <larinserg@gmail.com>
2024-04-07 18:18:08 +0700

    fix username generation

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
 from .forms import RegistrationForm
 import os
+
+User = get_user_model()  # Получаем модель пользователя
 
 
 def home(request):
@@ -9,16 +12,23 @@ def home(request):
 
 
 def register(request):
-    # Инициализируем форму регистрации
     if request.method == 'POST':
         form = RegistrationForm(request.POST, request.FILES)
         if form.is_valid():
-            # Если форма валидна, сохраняем нового пользователя
-            new_user = form.save(commit=False)
-            # Здесь можно добавить дополнительную логику обработки, если это необходимо
-            # Например, обработка или сохранение telegram_id из формы
-            new_user.telegram_id = form.cleaned_data.get('telegram_id', None)
-            new_user.save()
+            # Создаем пользователя с помощью create_user
+            user_kwargs = {
+                'password': None,  # Пароль будет сгенерирован автоматически, если не указан
+                'first_name': form.cleaned_data['first_name'],
+                'last_name': form.cleaned_data['last_name'],
+                'middle_name': form.cleaned_data.get('middle_name', ''),
+                'department_id': form.cleaned_data['department_id'],
+                'telegram_id': form.cleaned_data['telegram_id'],
+            }
+            # Добавляем email, только если он был предоставлен
+            if form.cleaned_data.get('email'):
+                user_kwargs['email'] = form.cleaned_data['email']
+
+            new_user = User.objects.create_user(**user_kwargs)
             # После успешной регистрации перенаправляем пользователя на главную страницу
             return redirect('users:home')
     else:
@@ -27,7 +37,6 @@ def register(request):
     # Определяем имя пользователя Telegram бота в зависимости от окружения
     bot_username = 'Event_dev_sgu_bot' if os.getenv('DJANGO_ENV') == 'development' else 'Event_sgu_bot'
 
-    # Передаем форму и имя пользователя бота в контекст для рендеринга в шаблоне
     context = {
         'form': form,
         'telegram_bot_username': bot_username,
