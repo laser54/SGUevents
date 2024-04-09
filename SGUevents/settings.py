1d074cdb51b3985b7181cdb825a3bff03cf495ce
LAlexanderS <likov.shura2003@mail.ru>
2024-04-09 17:39:56 +0700

    подключение автоматического заполнения карточек из админ панели и последующей бд

--- SGUevents/settings.py
+++ SGUevents/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'SGUevents.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
--- events_available/apps.py
+++ events_available/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class EventsAvailableConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'events_available'
+    verbose_name = 'Доступные мероприятия'
+    
