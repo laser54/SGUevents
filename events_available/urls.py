186755ef51190b478da2ae1f61795f7bcfafbb3d
LAlexanderS <likov.shura2003@mail.ru>
2024-04-14 20:22:06 +0700

    на этапе реализации перехода на индивидуальную карточку по id

--- events_available/urls.py
+++ events_available/urls.py
@@ -5,6 +5,8 @@ app_name = 'events_available'
 
 urlpatterns = [
   path('online/', views.online, name='online'),
+	path('online/event/<int:events_id>/', views.online_card, name='#'),
+
 	path('offline/', views.offline, name='offline'),
-	
+
 ]
