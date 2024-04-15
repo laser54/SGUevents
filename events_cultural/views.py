4a2aafe4df667a32369bd2db2d68c1d6953ea22a
LAlexanderS <likov.shura2003@mail.ru>
2024-04-15 15:46:06 +0700

    Настроен переход на индивидуальную карточку мероприятия по уникальному url адресу, автоматически заполняющегося при регистрации мероприятия в бд. Все мероприятия выгружаются на главную страницу (пока без возможности перехода по ссылке)

--- events_cultural/views.py
+++ events_cultural/views.py
@@ -12,6 +12,19 @@ def attractions(request):
 	}
 	return render(request, 'events_cultural/attractions.html', context)
 
+def attractions_card(request, event_slug=False, event_id=False):
+	event = Attractions.objects.all()
+
+	if event_id:
+		event = Attractions.objects.get(id=event_id)
+	else:
+		event = Attractions.objects.get(slug=event_slug)
+
+	context: dict[str, str] = {
+			'event': event,
+	}
+
+	return render(request, 'events_cultural/card.html', context=context)
 
 def events_for_visiting(request):
 	events_for_visiting_content = Events_for_visiting.objects.all()
@@ -22,6 +35,19 @@ def events_for_visiting(request):
 	}
 	return render(request, 'events_cultural/events_for_visiting.html', context)
 
+def for_visiting_card(request, event_slug=False, event_id=False):
+	event = Events_for_visiting.objects.all()
+	
+	if event_id:
+		event = Events_for_visiting.objects.get(id=event_id)
+	else:
+		event = Events_for_visiting.objects.get(slug=event_slug)
+
+	context: dict[str, str] = {
+			'event': event,
+	}
+
+	return render(request, 'events_cultural/card.html', context=context)
 
 def events_registered(request):
 	context: dict[str, str] = {
@@ -31,3 +57,17 @@ def events_registered(request):
 
 def index(request):
 	return render(request, 'events_cultural/index.html')
+
+# def registered_card(request, event_slug=False, event_id=False):
+# 	event = Events_online.objects.all()
+# 	# event = Events_online.objects.get(id=events_id)
+# 	if event_id:
+# 		event = Events_online.objects.get(id=event_id)
+# 	else:
+# 		event = Events_online.objects.get(slug=event_slug)
+
+# 	context: dict[str, str] = {
+# 			'event': event,
+# 	}
+
+# 	return render(request, 'events_cultural/card.html', context=context)
