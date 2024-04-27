e9a2872931319bb2da62ccc174c3ff2b9386cf92
la5er <larinserg@gmail.com>
2024-04-27 09:59:37 +0700

    bot bugfix

--- bot/django_initializer.py
+++ bot/django_initializer.py
@@ -1,9 +1,13 @@
-# django_initializer.py
 import os
 import django
+from django.core.wsgi import get_wsgi_application
 
 def setup_django_environment():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SGUevents.settings")
-    # В разработке вызываем setup, если приложения Django еще не загружены
-    if os.getenv('DJANGO_ENV') == 'development' and not django.apps.apps.ready:
-        django.setup()
+    if not django.apps.apps.ready:
+        if os.getenv('DJANGO_ENV') == 'production':
+            # В продакшене используем get_wsgi_application, которое делает setup автоматически
+            return get_wsgi_application()
+        else:
+            # В разработке просто делаем setup
+            django.setup()
