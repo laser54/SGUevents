0c4bd5d1e1bd7d223bd5d4a53701850d60f81d54
LAlexanderS <likov.shura2003@mail.ru>
2024-05-16 10:22:35 +0700

    Настроена полнотекстовый поиск  в онлайн мероприятиях. Выделяется текст в названии (разобраться с description и триграммным сходством)

--- events_available/utils.py
+++ events_available/utils.py
@@ -1,12 +1,35 @@
 from events_available.models import Events_online, Events_offline
 from django.db.models import Q
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector, SearchHeadline
 
 def q_search_online(query):
 	if query.isdigit() and len(query) <= 5:
 		return Events_online.objects.filter(id=int(query))
 	
-	return Events_online.objects.filter(description__search=query)
+	vector = SearchVector("name", "description")
+	query = SearchQuery(query)
+	result = Events_online.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
+	
+	result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
 
+	# result = result.annotate(
+    #     bodyline=SearchHeadline(
+    #         "description",
+    #         query,
+    #         start_sel='<span style="background-color: yellow;">',
+    #         stop_sel="</span>",
+    #     )
+    # )
+	return result
+
+    
 	# keyword = [word for word in query.split() if len(word) > 2]
 
 	# q_objects = Q()
