2e626b757535000a0c6d4c386ee8c5cdc731983b
la5er <larinserg@gmail.com>
2024-06-07 10:48:47 +0700

    rollback

--- bot/main.py
+++ bot/main.py
@@ -1,38 +1,28 @@
 import asyncio
 import logging
 import os
+import sys
 
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, types, F, Router
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import CommandStart
 from aiogram.enums import ParseMode
 from asgiref.sync import sync_to_async
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import StatesGroup, State
-from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.types import Message
+
 
 load_dotenv()
 from bot.django_initializer import setup_django_environment
 
 from django.contrib.auth import get_user_model
 from django.conf import settings
-from users.models import SupportRequest
-
 logging.basicConfig(level=logging.INFO)
 
 # Initialize bot
 TOKEN = settings.ACTIVE_TELEGRAM_BOT_TOKEN
-bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
-storage = MemoryStorage()
-dp = Dispatcher(storage=storage)
+dp = Dispatcher()
 
-# Define states
-class SupportRequestForm(StatesGroup):
-    waiting_for_question = State()
 
-# Router for handling support requests
-support_router = Router()
+
 
 async def get_user_profile(telegram_id):
     User = get_user_model()
@@ -72,35 +62,25 @@ def get_department_name(user):
     return user.department.department_name if user.department else "Не указан"
 
 @dp.message(F.text == "\U0001F5D3 Мои мероприятия")
-async def my_events(message: types.Message):
+async def without_puree(message: types.Message):
     await message.answer("Функция 'Мои мероприятия' еще не реализована. Подождите немного!")
 
-@support_router.message(F.text == "\U00002754 Помощь")
-async def help_request(message: types.Message, state: FSMContext):
-    await message.answer("Пожалуйста, введите ваш вопрос:")
-    await state.set_state(SupportRequestForm.waiting_for_question)
+@dp.message(F.text == "\U00002754 Помощь")
+async def without_puree(message: types.Message):
+    await message.answer("Функция 'Помощь' еще не реализована. Подождите немного!")
 
-@support_router.message(SupportRequestForm.waiting_for_question)
-async def receive_question(message: types.Message, state: FSMContext):
-    user = await get_user_profile(message.from_user.id)
-    if user:
-        # Сохраняем вопрос в базе данных
-        support_request = await sync_to_async(SupportRequest.objects.create)(
-            user=user,
-            question=message.text
-        )
-        await message.answer("Ваш вопрос отправлен в техподдержку. Спасибо!")
-    else:
-        await message.answer("Вы не зарегистрированы на портале.")
-    await state.clear()
-
-# Add the router to the dispatcher
-dp.include_router(support_router)
 
 # Функция запуска бота
+bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+
+
 async def run_bot():
-    setup_django_environment()
-    await dp.start_polling(bot)
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await bot.close()
+
 
 if __name__ == "__main__":
+    setup_django_environment()
     asyncio.run(run_bot())
