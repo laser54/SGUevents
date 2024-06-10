aa5a43687edef960c6ef436422d95570789cfcf4
la5er <larinserg@gmail.com>
2024-06-10 08:56:17 +0700

    Запись запроса из бота в БД

--- bot/main.py
+++ bot/main.py
@@ -4,10 +4,13 @@ import os
 import sys
 
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, types, F
+from aiogram import Bot, Dispatcher, types, F, Router
 from aiogram.filters import CommandStart
 from aiogram.enums import ParseMode
 from asgiref.sync import sync_to_async
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.storage.memory import MemoryStorage
 
 
 load_dotenv()
@@ -15,13 +18,16 @@ from bot.django_initializer import setup_django_environment
 
 from django.contrib.auth import get_user_model
 from django.conf import settings
+from users.models import SupportRequest
 logging.basicConfig(level=logging.INFO)
 
 # Initialize bot
 TOKEN = settings.ACTIVE_TELEGRAM_BOT_TOKEN
-dp = Dispatcher()
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
 
-
+class SupportRequestForm(StatesGroup):
+    waiting_for_question = State()
 
 
 async def get_user_profile(telegram_id):
@@ -66,9 +72,27 @@ async def without_puree(message: types.Message):
     await message.answer("Функция 'Мои мероприятия' еще не реализована. Подождите немного!")
 
 @dp.message(F.text == "\U00002754 Помощь")
-async def without_puree(message: types.Message):
-    await message.answer("Функция 'Помощь' еще не реализована. Подождите немного!")
+async def help_request(message: types.Message, state: FSMContext):
+    user = await get_user_profile(message.from_user.id)
+    if user:
+        await message.answer("Пожалуйста, введите ваш вопрос:")
+        await state.set_state(SupportRequestForm.waiting_for_question)
+    else:
+        await message.answer("Вы не зарегистрированы на портале.")
 
+@dp.message(SupportRequestForm.waiting_for_question)
+async def receive_question(message: types.Message, state: FSMContext):
+    user = await get_user_profile(message.from_user.id)
+    if user:
+        # Сохраняем вопрос в базе данных
+        support_request = await sync_to_async(SupportRequest.objects.create)(
+            user=user,
+            question=message.text
+        )
+        await message.answer("Ваш вопрос отправлен в техподдержку. Спасибо!")
+    else:
+        await message.answer("Вы не зарегистрированы на портале.")
+    await state.clear()
 
 # Функция запуска бота
 bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
