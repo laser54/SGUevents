501aea8e8a672b2d538c9afe0fcfe17b2617cf4a
La5er <larinserg@gmail.com>
2024-08-04 22:34:36 +0700

    workflow bugfix

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from main import views
+from . import views
 
 app_name = 'main'
 
