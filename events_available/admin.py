e37525239c551ad9045e6215f504e61f867bf424
LAlexanderS <likov.shura2003@mail.ru>
2024-09-04 14:27:37 +0700

    rights and groups

--- events_available/admin.py
+++ events_available/admin.py
@@ -2,48 +2,48 @@ from typing import Any
 from django.contrib import admin
 from django.db.models.query import QuerySet
 from django.http import HttpRequest
+from django.contrib.auth import get_user_model
 
 from events_available.models import Events_offline, Events_online
 
-@admin.register(Events_online)
-class Events_onlineAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',)}
-    filter_horizontal = ('secret',)
+User = get_user_model()
 
+# Проверяем, принадлежит ли пользователь к определенной группе
+def user_in_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()
+
+class RestrictedAdminMixin:
+    """
+    Миксин для ограничения прав редактирования и удаления объектов в зависимости от того,
+    является ли пользователь администратором мероприятия.
+    """
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         if request.user.is_superuser:
-            return qs 
+            return qs  # Суперпользователь видит все объекты
+        # Пользователь видит только те мероприятия, где он администратор
         return qs.filter(events_admin=request.user.pk)
 
     def has_change_permission(self, request, obj=None):
         if obj is not None and not request.user.is_superuser:
-            return obj.events_admin == request.user.username
+            # Разрешаем редактирование только если пользователь администратор мероприятия
+            return obj.events_admin == request.user
         return super().has_change_permission(request, obj)
 
     def has_delete_permission(self, request, obj=None):
         if obj is not None and not request.user.is_superuser:
-            return obj.events_admin == request.user.username
+            # Разрешаем удаление только если пользователь администратор мероприятия
+            return obj.events_admin == request.user
         return super().has_delete_permission(request, obj)
+
+# Настройка админ-панели для каждой модели
+@admin.register(Events_online)
+class Events_onlineAdmin(RestrictedAdminMixin, admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    filter_horizontal = ('secret',)
 
 
 @admin.register(Events_offline)
-class Events_offlineAdmin(admin.ModelAdmin):
+class Events_offlineAdmin(RestrictedAdminMixin, admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     filter_horizontal = ('secret',)
-    
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        if request.user.is_superuser:
-            return qs
-        return qs.filter(events_admin=request.user.username)
-
-    def has_change_permission(self, request, obj=None):
-        if obj is not None and not request.user.is_superuser:
-            return obj.events_admin == request.user.username
-        return super().has_change_permission(request, obj)
-
-    def has_delete_permission(self, request, obj=None):
-        if obj is not None and not request.user.is_superuser:
-            return obj.events_admin == request.user.username
-        return super().has_delete_permission(request, obj)
--- events_cultural/admin.py
+++ events_cultural/admin.py
@@ -2,48 +2,45 @@ from django.contrib import admin
 from django.utils.timezone import localtime
 from pytz import timezone as pytz_timezone
 from events_cultural.models import Attractions, Events_for_visiting, Review
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# Проверяем, принадлежит ли пользователь к определенной группе
+def user_in_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()
+
+class RestrictedAdminMixin:
+    """
+    Миксин для ограничения прав редактирования и удаления объектов в зависимости от того,
+    является ли пользователь администратором мероприятия.
+    """
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs  # Суперпользователь видит все объекты
+        # Пользователь видит только те мероприятия, где он администратор
+        return qs.filter(events_admin=request.user.pk)
+
+    def has_change_permission(self, request, obj=None):
+        if obj is not None and not request.user.is_superuser:
+            # Разрешаем редактирование только если пользователь администратор мероприятия
+            return obj.events_admin == request.user
+        return super().has_change_permission(request, obj)
+
+    def has_delete_permission(self, request, obj=None):
+        if obj is not None and not request.user.is_superuser:
+            # Разрешаем удаление только если пользователь администратор мероприятия
+            return obj.events_admin == request.user
+        return super().has_delete_permission(request, obj)
 
 @admin.register(Attractions)
-class AttractionsAdmin(admin.ModelAdmin):
+class AttractionsAdmin(RestrictedAdminMixin, admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
-    filter_horizontal = ('secret',)
-
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        if request.user.is_superuser:
-            return qs
-        return qs.filter(events_admin=request.user.username)
-
-    def has_change_permission(self, request, obj=None):
-        if obj is not None and not request.user.is_superuser:
-            return obj.events_admin == request.user.username
-        return super().has_change_permission(request, obj)
-
-    def has_delete_permission(self, request, obj=None):
-        if obj is not None and not request.user.is_superuser:
-            return obj.events_admin == request.user.username
-        return super().has_delete_permission(request, obj)
 
 @admin.register(Events_for_visiting)
-class Events_for_visitingAdmin(admin.ModelAdmin):
+class Events_for_visitingAdmin(RestrictedAdminMixin, admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
-    filter_horizontal = ('secret',)
-
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        if request.user.is_superuser:
-            return qs
-        return qs.filter(events_admin=request.user.username)
-
-    def has_change_permission(self, request, obj=None):
-        if obj is not None and not request.user.is_superuser:
-            return obj.events_admin == request.user.username
-        return super().has_change_permission(request, obj)
-
-    def has_delete_permission(self, request, obj=None):
-        if obj is not None and not request.user.is_superuser:
-            return obj.events_admin == request.user.username
-        return super().has_delete_permission(request, obj)
 
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('formatted_date_submitted', 'user', 'event', 'comment')
